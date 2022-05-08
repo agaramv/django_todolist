b2a57ded75153c9d2ed8fbfa1a730db32e84e66f
agaramv <42156415+agaramv@users.noreply.github.com>
2022-05-08 15:55:55 -0400

    Added in Class based views

--- tasklist/urls.py
+++ tasklist/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
   path('', views.apiOverview, name="api-overview"),
-  path('task/all/', views.taskList, name="task-list"),
-  path('task/view/<str:id>/', views.taskView, name="task-view"),
-  path('task/update/<str:id>/', views.taskUpdate, name="task-update"),
+  path('task/all/', views.TaskListAll.as_view(), name="task-list"),
+  # path('task/view/<str:id>/', views.taskView, name="task-view"),
+  # path('task/update/<str:id>/', views.taskUpdate, name="task-update"),
+  # path('task/create/', views.taskCreate, name="task-create"),
+  # path('task/delete/<str:pk>/', views.taskDelete, name="task-delete"),
+  # path('task/universal/<str:pk>/', views.TaskListUniversal.as_view(), name="task-univesal")
+  path('task/<str:pk>', views.TaskListDetail.as_view(), name="task-detail")
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- tasklist/views.py
+++ tasklist/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.decorators import api_view, APIView
 from rest_framework.response import Response
 from .serializers import TaskListSerializer
 from .models import TaskList
+from rest_framework import generics
+
 # API Overview
 
 @api_view(['GET'])
@@ -40,3 +43,67 @@ def taskUpdate(request, id):
   if serializer.is_valid():
     serializer.save()
   return Response(serializer.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+  print(request) 
+  # serializer is taking the data given in response and converting the request data through the task list serializer into jSON
+  serializer = TaskListSerializer(data=request.data)
+  if serializer.is_valid():
+    serializer.save()
+  print(serializer)
+  return Response(serializer.data)
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+  # print(request) 
+  task = TaskList.objects.get(id=pk)
+  # print(task)
+  # Find out how to do error handling with invalid delete
+  # No need for serializer bc the task data isn't being converted from or to json
+  task.delete()
+  return Response("Task Deleted Successfully")
+
+class TaskListAll(generics.ListCreateAPIView):
+    queryset = TaskList.objects.all()
+    serializer_class = TaskListSerializer
+
+
+class TaskListDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TaskList.objects.all()
+    serializer_class = TaskListSerializer
+
+class TaskListUniversal(APIView):
+  """
+    List all snippets, or create a new snippet.
+  """
+  def get_obj(self, pk):
+    try:
+      return TaskList.objects.get(id=pk)
+    except TaskList.DoesNotExist:
+      return Response(status = status.HTTP_404_NOT_FOUND)
+
+  def get(self, request, pk, format=None):
+      tasks = TaskList.objects.all()
+      serializer = TaskListSerializer(tasks, many=True)
+      return Response(serializer.data)
+  
+  def post(self, request, pk, format=None):
+    serializer = TaskListSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+    print(serializer)
+    return Response(serializer.data)
+
+  def put(self, request, pk, format=None):
+    task = TaskList.get_obj(pk)
+    serializer = TaskListSerializer(instance=task, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk, format=None):
+    task = TaskList.get_obj(pk)
+    task.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
