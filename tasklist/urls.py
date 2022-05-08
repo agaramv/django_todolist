01d906247747dc0ab6da4c8ba66501e77516774c
agaramv <42156415+agaramv@users.noreply.github.com>
2022-05-07 21:12:53 -0400

    TaskList: Made get by id and update by id

--- tasklist/urls.py
+++ tasklist/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.apiOverview, name="api-overview"),
   path('task/all/', views.taskList, name="task-list"),
-  
+  path('task/view/<str:id>/', views.taskView, name="task-view"),
+  path('task/update/<str:id>/', views.taskUpdate, name="task-update"),
 ]
--- tasklist/views.py
+++ tasklist/views.py
@@ -9,6 +9,7 @@ from .models import TaskList
 def apiOverview(request):
   api_urls = {
     'List' : '/task/all',
+    'View-Task': '/task/view/<str:pk>',
     'Create': '/task/create',
     'Update': '/task/update/<str:pk>',
     'Delete': '/task/delete/<str:pk>'
@@ -19,4 +20,23 @@ def apiOverview(request):
 def taskList(request):
   taskList = TaskList.objects.all()
   serializer = TaskListSerializer(taskList, many = True)
+  return Response(serializer.data)
+
+"""
+This Function going to display Detailed view of one perticuler task with the help of pk.
+"""
+@api_view(['GET'])
+def taskView(request, id):
+  print(request," ID: ",id)
+  task = TaskList.objects.get(id=id)
+  serializer = TaskListSerializer(task, many = False)
+  print(serializer)
+  return Response(serializer.data)
+
+@api_view(['PUT'])
+def taskUpdate(request, id):
+  task = TaskList.objects.get(id=id)
+  serializer = TaskListSerializer(instance=task, data=request.data)
+  if serializer.is_valid():
+    serializer.save()
   return Response(serializer.data)
